7b15329f5016794128846b448878774f211b1c04
ninh.nguyentan <nguyentanninh123@gmail.com>
2025-08-06 15:09:24 +0700

    c

--- src/EnterpriseAuth.Domain/obj/Debug/net9.0/EnterpriseAuth.Domain.AssemblyInfo.cs
+++ src/EnterpriseAuth.Domain/obj/Debug/net9.0/EnterpriseAuth.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseAuth.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ca4349e31b4eb2fad6e8287e6b437e46d76010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440ec215f327a32cd0b248e0f9003e945f2ff180")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseAuth.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseAuth.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
