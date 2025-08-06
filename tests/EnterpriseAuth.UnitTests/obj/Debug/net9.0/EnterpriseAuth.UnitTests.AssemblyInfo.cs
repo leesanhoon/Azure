4abd9f9cadc825e7b3699846af83e396381cdda0
ninh.nguyentan <nguyentanninh123@gmail.com>
2025-08-06 13:41:30 +0700

    Update assembly informational version and cache files for all projects

--- tests/EnterpriseAuth.UnitTests/obj/Debug/net9.0/EnterpriseAuth.UnitTests.AssemblyInfo.cs
+++ tests/EnterpriseAuth.UnitTests/obj/Debug/net9.0/EnterpriseAuth.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseAuth.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a23941e85ee4b3388702de3003f738ae04929c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseAuth.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseAuth.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
